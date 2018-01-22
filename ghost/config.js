fff60d929381be998745621b4dca567c7698f235
jiajia.tang <jiajia.tang@baidao.com>
2018-01-22 17:57:56 +0800

    add project

---
+++ ghost/config.js
@@ -0,0 +1,31 @@
+var path = require('path')
+var config
+
+
+config = {
+  production: {
+    url: 'http://mytestblog.com',
+    mail: {},
+    database: {
+      client: 'mysql',
+      connection: {
+        host: 'db',
+        user: 'ghost',
+        password: 'ghost',
+        databse: 'ghost',
+        port: '3306',
+        charset: 'utf8' 
+      },
+      debug: false
+    },
+    paths: {
+      contentPath: path.join(process.env.GHOST_CONTENT, '/') 
+    },
+    server: {
+      host: '0.0.0.0',
+      port: '2368'  
+    }
+  }
+}
+
+module.exports = config
